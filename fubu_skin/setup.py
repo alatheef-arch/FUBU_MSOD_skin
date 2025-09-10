535cf1e392306eaea247768d372ac66f4cb22e99
Arshiya Abdul Latheef <arshiya-abdul.latheef.external@airbus.com>
2025-09-10 14:04:57 +0530

    Create setup.py

---
+++ fubu_skin/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+# For the skin repo, the name should be 'fubu_skin'
+# For the stringer repo, the name should be 'fubu_stringer'
+setup(
+    name='fubu_skin',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A dynamic plugin for the FUBU MSOD application.'
+)
